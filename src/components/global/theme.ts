4a98c24ef0935f7abb55477309861693677375c2
Sarah Hoare <sarah.sioux.h.07@gmail.com>
2024-04-18 12:55:42 -0700

    gave up on font

--- src/components/global/theme.ts
+++ src/components/global/theme.ts
@@ -1,9 +1,9 @@
 'use client';
 import { createTheme } from '@mui/material/styles';
-import { GFS Didot } from '../../../assets/fonts/GFSDidot-Regular.ttf';
 
 const theme = createTheme({
     typography: {
+
 
     },
     palette: {
@@ -11,7 +11,6 @@ const theme = createTheme({
             light: "#9BD7D0",
             main: "#5FA9A0",
             dark: "#3B8377",
-
         },
     }
 })
